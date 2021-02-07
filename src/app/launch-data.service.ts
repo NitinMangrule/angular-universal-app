5448467d2637c52ecfd0af72539f3d16f018de85
Nitin Mangrule <ndmangrule@gmail.com>
2021-02-07 18:58:39 +0530

    Update launch-data.service.ts

--- src/app/launch-data.service.ts
+++ src/app/launch-data.service.ts
@@ -1,5 +1,7 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { Observable, throwError } from 'rxjs';
+import { retry, catchError } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
@@ -8,11 +10,11 @@ export class LaunchDataService {
   baseURL: string = 'https://api.spaceXdata.com/v3/launches?limit=100';
   constructor(private http: HttpClient) { }
 
-  getAllLaunchDetails() {
+  getAllLaunchDetails(): Observable<any> {
     return this.http.get(this.baseURL);
   }
 
-  getLaunchDetailsByFilter(filterData: any) {
+  getLaunchDetailsByFilter(filterData: any): Observable<any> {
     let updatedURL = '';
     switch (filterData.filterBy) {
       case 'filterByYear':
@@ -32,7 +34,21 @@ export class LaunchDataService {
         break;
     }
 
-    return this.http.get(updatedURL);
+    return this.http.get(updatedURL).pipe(
+      retry(1),
+      catchError(this.handleError)
+    );
+  }
+
+  handleError(error) {
+    let errorMessage = '';
+    if (error.error instanceof ErrorEvent) {
+      errorMessage = `Error: ${error.error.message}`;
+    } else {
+      errorMessage = `Error Code: ${error.status}\nMessage: ${error.message}`;
+    }
+    window.alert(errorMessage);
+    return throwError(errorMessage);
   }
 
 }
